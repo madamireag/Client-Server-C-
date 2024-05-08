6f06ebfff345b1fbf1a5fc24520c03962efd3a34
Mireag <andreea.mireag@siemens-energy.com>
2024-05-08 15:47:17 +0300

    Add project files.

---
+++ Client1/Program.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client1
+{
+    class Program
+    {
+        public static TcpClient tcpclnt = new TcpClient();
+
+        static void Main(string[] args)
+        {
+            Connect();
+            //Console.WriteLine("Options:\n 1. Write a message \n 2. Close connection");
+            //Int32.TryParse(Console.ReadLine(), out int option);
+
+            LoopPacket();
+            //switch (option)
+            //    {
+            //        case 1: { LoopPacket();
+                            
+            //                break;
+            //            }
+            //        case 2: { tcpclnt.Close();
+            //                  Console.WriteLine("Connection closed!");
+            //                break;
+            //            }
+                
+            //    }
+                
+            tcpclnt.Close();
+            }
+        
+        private static bool IsFileLocked(string filePath)
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = File.Open(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);  
+                stream.Close();
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private static void LoopPacket()
+        {
+            byte[] bytesFrom = new byte[4096];
+            string dataFromServer = null;
+            byte[] sendBytes = null;
+            string clientRequest = null;
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            IConfiguration config = builder.Build();
+
+            // de adaugat logica sa-i dea de ales daca vrea sa scrie mesaj sau sa se deconecteze
+
+            while (true) { 
+                try
+                {
+                    string filePath = config["filePath"];
+                    int.TryParse(config["clientTimeout"], out int timeout);
+
+                    if (File.Exists(filePath) && !IsFileLocked(filePath))
+                    {
+                        //using (StreamWriter writer = new StreamWriter(filePath, true))
+                        //{
+                        //    // citesc de la consola ce vreau sa scriu in fisier
+                        //    Console.WriteLine("Write a message:");
+                        //    string input = Console.ReadLine();
+
+                        //    //scriu in fisier
+                        //    //if(int.Parse(input) == 0)
+                        //    //    break;
+                        //    writer.WriteLine(input);
+
+                        //    //inchid fisierul/stream-ul
+                        //    writer.Close();
+                        //}
+
+                        // de adaugat citirea ciclica din fisier
+                        // cum stiu ca e mesajul pt mine? - convin ca ultimul cuvant e destinatarul
+                        // unde dau message received si message processed?
+                        // ciclu configurabil -> ? 
+                        // cum fac sa nu procesez acelasi mesaj de mai multe ori? -> dupa ce-l citesc din fisier il sterg?
+
+                        NetworkStream networkStream = tcpclnt.GetStream();
+                        networkStream.Read(bytesFrom, 0, 4096);
+                        dataFromServer = Encoding.ASCII.GetString(bytesFrom);
+                        dataFromServer = dataFromServer.Substring(0, dataFromServer.IndexOf("\0"));
+                        Console.WriteLine("Server: " + dataFromServer);
+                        networkStream.Flush();
+
+
+                    } else if (IsFileLocked(filePath))
+                    {
+                        Console.WriteLine($"The file is locked! Retrying in {timeout / 60000} min");
+                    }
+
+
+                    Thread.Sleep(timeout);
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Error: " + ex.Message);
+                    break;
+                }
+                //Console.WriteLine("Options:\n 1. Write a message \n 2. Close connection");
+                //int option = Int32.Parse(Console.ReadLine());
+
+            }
+        }
+
+        private static void Connect()
+        {
+            Console.WriteLine("Connecting...");
+            //while (true)
+            //{
+                try
+                {
+                    tcpclnt.Connect(IPAddress.Parse("127.0.0.1"), 9999);
+                    if (tcpclnt.Connected)
+                    {
+                        Console.WriteLine("Connected!");
+                    }
+                //  break;
+                }
+                catch (Exception ex)
+                {
+                    // Handle connection errors (retry logic, etc.)
+                    Console.WriteLine("Error connecting: " + ex.Message);
+                 //   System.Threading.Thread.Sleep(1000); // Wait before retrying
+                }
+            }
+        }
+    }
+
+
---
+++ Server/Program.cs
@@ -0,0 +1,166 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+   class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+            main.server_start();
+
+            Console.ReadLine();
+        }
+
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
+
+        private void server_start()
+        {
+            server.Start();
+            Console.WriteLine("Server Started");
+            accept_connection();  //accepts incoming connections
+        }
+
+        private void accept_connection()
+        {
+            server.BeginAcceptTcpClient(handle_connection, server);  //this is called asynchronously and will run in a different thread
+        }
+
+        private static bool IsFileLocked(string filePath)
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+
+            if (stream != null)
+                stream.Close();
+
+            return false;
+        }
+
+        private void handle_connection(IAsyncResult result)  //the parameter is a delegate, used to communicate between threads
+        {
+          
+
+            accept_connection();  //once again, checking for any other incoming connections
+            TcpClient client = server.EndAcceptTcpClient(result);  //creates the TcpClient
+
+            string clientIP = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+            int clientPort = ((IPEndPoint)client.Client.RemoteEndPoint).Port;
+            Console.WriteLine(clientIP +":"+ clientPort+" connected");
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+
+            string filePath = config["filePath"] ;
+            int.TryParse(config["serverTimeout"], out int timeout);
+
+
+            // de adaugat aici citit la ciclul configurat
+            while (true)
+            {
+                try
+                {
+                    if (File.Exists(filePath) && !IsFileLocked(filePath))
+                    {
+                        string[] linesArr =
+                            File.ReadAllLines(filePath);
+                        List<string> listOfLines = new List<string>();
+                        listOfLines.AddRange(linesArr);
+                        List<string> remainingLines = new List<string>();
+
+                        //sterg mesajele procesate ca sa nu le citesc de 2 ori si trimit confirmarea clientului
+                        foreach (string l in listOfLines)
+                        {
+                            string[] message = l.Split(" ", StringSplitOptions.None);
+                            if (message[message.Length - 1].ToLower().Equals("server"))
+                            {
+                                // ii trimit confirmare clientului 
+                                NetworkStream ns = client.GetStream();
+                                byte[] messageInBytes = new byte[4096];
+                                messageInBytes = Encoding.ASCII.GetBytes("Message received and processed!");
+                                ns.Write(messageInBytes, 0, messageInBytes.Length);
+                                //scriu ce mesaje am primit
+                                Console.WriteLine($"Message received: {l}");
+                            }
+                            else { remainingLines.Add(l); }
+                        }
+
+                        File.WriteAllLines(filePath, remainingLines.ToArray());
+
+                        //using (StreamReader file = new StreamReader(filePath))
+                        //{
+                        //    // citire linie cu linie din fisier
+                        //    string line;
+
+                        //    while ((line = file.ReadLine()) != null)
+                        //    {
+                        //        // ce trebuie sa faca cand citeste mesajele?
+                        //        string[] message = line.Split(" ", StringSplitOptions.None);
+                        //        if (message[message.Length - 1].ToLower().Equals("server"))
+                        //        {
+                        //            // mesajul e pt mine (server), trebuie sa trimit clientului feedback
+                        //            // trimit tot prin fisier sau ii scriu in consola?
+
+                        //            //NetworkStream ns = client.GetStream();
+
+                        //            //byte[] message = new byte[4096];
+                        //            //string clientRequest = "Message received!";
+                        //            //message = Encoding.ASCII.GetBytes(clientRequest);
+                        //            //ns.Write(message, 0, message.Length);
+
+                        //            //sterg mesajul ca sa nu-l citesc de 2 ori
+                        //            string[] linesArr = File.ReadAllLines(filePath);
+                        //            List<string> listOfLines = new List<string>();
+                        //            listOfLines.AddRange(linesArr);
+
+                        //            listOfLines.Remove(line);
+
+                        //            File.WriteAllLines(filePath,listOfLines.ToArray());
+
+                        //        }
+
+
+                        //    }
+                        //    file.Close();
+                        //}
+
+                        Thread.Sleep(timeout);
+
+                    }
+                    else if (IsFileLocked(filePath))
+                    {
+                        Console.WriteLine($"The file is locked! Retrying in {timeout / 60000} min");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+         
+
+        }
+
+    }
+}
